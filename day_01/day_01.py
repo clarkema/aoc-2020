56e83a26f05450cc15aae252be9769ae9bff7001
Mike Clarke <clarkema@clarkema.org>
2020-12-01 23:00:01 +0000

    Add a more general solution for day 1

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,17 +1,28 @@
 #! /usr/bin/env python
 
 from itertools import combinations
+from functools import reduce
+import operator
 
 with open("input") as fh:
     data = [int(l) for l in fh.readlines()]
 
 # Part 1, the straightforward way
-for i, x in enumerate(data):
-    for y in data[i+1:]:
-        if x + y == 2020:
-            print(f"{x} and {y} sum to 2020. Their product is {x * y}")
+def part_1():
+    for i, x in enumerate(data):
+        for y in data[i+1:]:
+            if x + y == 2020:
+                print(f"{x} and {y} sum to 2020. Their product is {x * y}")
 
-# Part 2, using itertools
-for (x, y, z) in combinations(data, 3):
-    if x + y + z == 2020:
-        print(f"{x}, {y}, and {z} sum to 2020. Their product is {x * y * z}")
+
+# General solution using itertools
+def expense(target, n):
+    for comb in combinations(data, n):
+        if sum(comb) == target:
+            product = reduce(operator.mul, comb, 1)
+            print(f"{comb} sum to {target}. The product is {product}")
+            return product
+    print(f"No solution found for {target} over {n} numbers")
+
+expense(2020, 2)
+expense(2020, 3)
